98b3063efb86319febf60abb585dda8dd3d263b7
CadeG <chgatewo@uark.edu>
2017-03-14 18:33:30 -0500

    Add project files.

---
+++ ConfigurableIrcBotApp/IrcClient.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Net.Sockets;
+
+using System.Threading;
+
+using System.Diagnostics;
+
+namespace ConfigurableIrcBotApp
+{
+    class IrcClient
+    {
+        private string userName;
+        private string channel;
+        private string password;
+
+        private string ip = "irc.twitch.tv";
+        private int port = 6667;
+
+        private TcpClient tcpClient;
+        private StreamReader inputStream;
+        private StreamWriter outputStream;
+
+        private Thread ircThread;
+        private bool _running;
+
+        private string motd = "placeholder";
+        private string streamInfo = "placeholder";
+
+        public IrcClient(MainWindow main, string userName, string password, string channel)
+        {
+            ircThread = new Thread(new ThreadStart(Run));
+            ircThread.IsBackground = true;
+
+            this.userName = userName;
+            this.password = password;
+            this.channel = channel;
+
+        }
+
+        public void Run()
+        {
+            this._running = true;
+
+            tcpClient = new TcpClient(ip, port);
+            inputStream = new StreamReader(tcpClient.GetStream());
+            outputStream = new StreamWriter(tcpClient.GetStream()) { NewLine = "\r\n", AutoFlush = true };
+
+            outputStream.WriteLine("PASS " + password);
+            outputStream.WriteLine("NICK " + userName);
+            outputStream.WriteLine("USER " + userName + " 8 * :" + userName);
+            joinRoom(channel);
+            outputStream.Flush();
+
+            while (_running)
+            {
+                parseMessage(readMessage());    
+            }
+        }
+
+        public void Start()
+        {
+            ircThread.Start();
+        }
+
+        public void Stop()
+        {
+            this._running = false;
+        }
+
+
+
+        public void joinRoom(string channel)
+        {
+            this.channel = channel;
+            outputStream.WriteLine("JOIN #" + channel);
+            outputStream.Flush();
+        }
+
+        public void sendIrcMessage(String message)
+        {
+            outputStream.WriteLine(message);
+        }
+
+        public void sendChatMessage(string message)
+        {
+            sendIrcMessage(":" + userName + "!" + userName + "@" + userName + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
+
+        public string readMessage()
+        {
+            string message = inputStream.ReadLine();
+            return message;
+        }
+
+        public void parseMessage(string message)
+        {
+            //todo proof of concept, more robust message object planned, smarter parsing planned
+            Trace.WriteLine(message);
+            if (message.Contains("!hello")){
+                sendChatMessage("Hi there!");
+            }
+            else if (message.Contains("!motd"))
+            {
+                sendChatMessage(motd);
+            }
+            else if (message.Contains("!stream"))
+            {
+                sendChatMessage(streamInfo);
+            }
+        }
+
+        public Boolean isRunning()
+        {
+            return _running;
+        }
+
+        public void setMotd(String message)
+        {
+            this.motd = message;
+        }
+
+        public void setStreamInfo(string message)
+        {
+            this.streamInfo = message;
+        }
+
+    }
+}
---
+++ ConfigurableIrcBotApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;
+
+namespace ConfigurableIrcBotApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private IrcClient bot;
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (userNameInput.Text != "" &&
+                passwordInput.Text != "" &&
+                channelInput.Text != ""
+                )
+            {
+                if (bot == null || (bot != null && !bot.isRunning()))
+                {
+                    bot = new IrcClient(this, userNameInput.Text, passwordInput.Text, channelInput.Text);
+                    bot.Start();
+                }
+                else if (bot.isRunning())
+                {
+                    MessageBox.Show("You already have a bot running, please disconnect the first before attempting a new connection");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the relevant information");
+            }
+
+        }
+
+        private void disconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            bot.sendChatMessage("bye!");
+            bot.Stop();
+        }
+
+        private void motdButton_Click(object sender, RoutedEventArgs e)
+        {
+            bot.setMotd(motdInput.Text);
+        }
+
+        private void streamInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            bot.setStreamInfo(streamInfoInput.Text);
+        }
+
+        private void sendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            bot.sendChatMessage(sendMessageInput.Text);
+        }
+    }
+}
